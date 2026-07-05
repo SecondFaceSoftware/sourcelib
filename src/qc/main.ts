5f70b54823179e93f4e8d6cfd81c4655f8174b5a
Stefan Heinz <stefan.heinz.02@gmail.com>
2026-07-05 17:22:22 +0200

    Expose more stuff from QC

--- src/qc/main.ts
+++ src/qc/main.ts
@@ -1,4 +1,5 @@
 import { QcTokenizer } from "./QcTokenizer.js";
 import { QcParser } from "./QcParser.js";
+import { QcCommand, QcDocument, QcCommandContent } from "./QcCommands.js";
 
-export { QcTokenizer, QcParser };
+export { QcTokenizer, QcParser,  QcCommand, QcDocument, QcCommandContent };
